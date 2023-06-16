7d6466fa28480ae1bcb9d7a3b6d941537dd39738
sampete1997 <sampete1997>
2023-06-16 21:00:00 +0530

    added reset and update time fun

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import './App.css';
 import { useEffect, useState } from 'react';
 import ScreenTimer from './components/ScreenTimer';
@@ -5,24 +6,41 @@ import ScreenTimer from './components/ScreenTimer';
 let screenTimer: any;
 
 export default function App() {
+  const timeOut: number =
+    Number(window.localStorage.getItem('timeOut')) || 60000; // default 60 sec
   const [isActive, setIsActive] = useState<boolean>(true);
-  const [second, setSecond] = useState<number>(0);
+  const [second, setSecond] = useState<number>(1);
   const [minute, setMinute] = useState<number>(0);
   const [hour, setHour] = useState<number>(0);
+  const [newTimeOut, setNewTimeOut] = useState(timeOut);
+  const [restartCount, setRestartCount] = useState<number>(0);
+
+  console.log('timeOut', timeOut);
+
+  useEffect(() => {
+    clearTimeout(screenTimer);
+    setRestartCount(0);
+    screenTimer = setTimeout(() => {
+      setIsActive(false);
+      document.body.style.backgroundColor = 'black';
+    }, newTimeOut);
+  }, [newTimeOut]);
 
   const handleUserActivity = () => {
     clearTimeout(screenTimer);
     setIsActive(true);
     document.body.style.backgroundColor = 'white';
+    setRestartCount(0);
     screenTimer = setTimeout(() => {
       setIsActive(false);
       document.body.style.backgroundColor = 'black';
-    }, 10000);
+    }, newTimeOut);
   };
 
   useEffect(() => {
     document.addEventListener('keydown', handleUserActivity);
     const screenStartTime = window.setInterval(() => {
+      setRestartCount(restartCount + 1);
       if (isActive) {
         if (second >= 59) {
           setSecond(0);
@@ -41,19 +59,31 @@ export default function App() {
       document.removeEventListener('keydown', handleUserActivity);
       clearInterval(screenStartTime);
     };
-  }, [second, isActive]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [second, isActive, restartCount]);
 
-  const isTimeFormat = (value: string) => {
+  const convertValidTimeFormat = (value: string) => {
     if (value.length < 2) {
       return '0'.concat(value);
     }
     return value;
   };
+
+  const handleReset: any = () => {
+    window.localStorage.setItem('timeOut', '60000');
+  };
+
   const finalTimeFormat = () => {
-    const sec = isTimeFormat(second.toString());
-    const min = isTimeFormat(minute.toString());
-    const hr = isTimeFormat(hour.toString());
+    const sec = convertValidTimeFormat(second.toString());
+    const min = convertValidTimeFormat(minute.toString());
+    const hr = convertValidTimeFormat(hour.toString());
     return `${hr}: ${min}: ${sec}`;
+  };
+
+  const getNewTimeOut = (value: number) => {
+    const time = 10000 * 6 * value;
+    window.localStorage.setItem('timeOut', time.toString());
+    setNewTimeOut(time);
   };
 
   return (
@@ -64,7 +94,13 @@ export default function App() {
       onMouseMove={handleUserActivity}
     >
       {isActive ? (
-        <ScreenTimer finalTimeFormat={finalTimeFormat()} />
+        <ScreenTimer
+          oldTime={timeOut}
+          getNewTimeOut={getNewTimeOut}
+          finalTimeFormat={finalTimeFormat()}
+          second={restartCount}
+          handleReset={handleReset}
+        />
       ) : (
         <div className="screenSaver">I Am Sleeping 😴</div>
       )}
--- src/renderer/components/ScreenTimer.tsx
+++ src/renderer/components/ScreenTimer.tsx
@@ -1,21 +1,120 @@
-import { memo } from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import { memo, useState } from 'react';
 import Draggable from 'react-draggable';
 import '../App.css';
 
 interface ActiveTimerProps {
   finalTimeFormat: string;
+  getNewTimeOut: (newTime: number) => void;
+  oldTime: number;
+  second: number;
+  handleReset: () => void;
 }
 
 function ScreenTimer(props: ActiveTimerProps) {
-  const { finalTimeFormat } = props;
+  const { finalTimeFormat, oldTime, getNewTimeOut, second, handleReset } =
+    props;
+  const [showOption, setShowOption] = useState(false);
+  const [inputValue, setInputValue] = useState(oldTime/60000);
+  const [showSetting, setShowSetting] = useState('none');
+  const [newTime, setNewTime] = useState(oldTime);
+  const [position, setPostion] = useState({
+    top: '0px',
+    left: '0px',
+  });
+  console.log('sec', second);
+  const handleRightClick = (e: any) => {
+    e.preventDefault();
+
+    setShowOption(true);
+    setPostion({
+      top: `${e.pageY}px`,
+      left: `${e.pageX}px`,
+    });
+  };
+
+  const handleOnClose = () => {
+    setShowOption(false);
+    setShowSetting('none');
+  };
+
+  const handleSetting = () => {
+    setShowOption(true);
+    setShowSetting('flex');
+  };
+
+  const handleEnter = (e: any) => {
+    if (e.key === 'Enter') {
+      getNewTimeOut(newTime);
+      setShowSetting('none');
+      setShowOption(false);
+      alert(`Screen time out updated to ${newTime} min`);
+    }
+  };
+
+  const handleChange = (e: any) => {
+    const { value } = e.target;
+    if (!value.match(/\d+/g) || value === '0') {
+      alert('Time should be positive number only');
+      setInputValue(oldTime/60000);
+      return;
+    }
+    setNewTime(Number(value));
+  };
   return (
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
     <div className="container">
+      <p className="timeOutSession">
+        Screen Timeout In {oldTime / 1000 - second} sec
+      </p>
       <Draggable bounds="parent">
-        <div className="box">
-          <p>Screen Time</p>
+        <div className="box" onContextMenu={handleRightClick}>
+          <p>Screen Time </p>
           <p>{finalTimeFormat || ''} </p>
         </div>
       </Draggable>
+      {showOption ? (
+        <div
+          className="rightClickPrompt"
+          style={{
+            top: position.top,
+            left: position.left,
+          }}
+        >
+          <div className="settingWrapper">
+            <div>
+              <div className="setting" onMouseDownCapture={handleSetting}>
+                Set screen time out
+              </div>
+              <div className="setting" onMouseDown={handleReset}>
+                reset Timer
+              </div>
+              <div className="setting">Theme</div>
+            </div>
+            <div onMouseDown={handleOnClose}>❎</div>
+          </div>
+
+          <div
+            style={{
+              top: position.top,
+              left: position.left,
+              display: showSetting,
+            }}
+          >
+            <div className="subClickPrompt">
+              <input
+                className="setScreenTime"
+                placeholder="Add time(min) & enter"
+                onKeyDown={handleEnter}
+                onChange={handleChange}
+                defaultValue={inputValue}
+              />
+            </div>
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   );
 }
